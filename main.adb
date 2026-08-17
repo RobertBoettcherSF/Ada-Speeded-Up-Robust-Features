cebd3ce691cb2d873a38b12e79ce5bff6e5e4387
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-17 16:57:42 +0200

    Add main.adb for SURF Algorithm Library

---
+++ main.adb
@@ -0,0 +1,7 @@
+-- main.adb
+with Ada.Text_IO; use Ada.Text_IO;
+procedure Main is
+begin
+   Put_Line ("SURF Algorithm Library Compiled.");
+   Put_Line ("Run 'make test' to execute V&V test suite.");
+end Main;
